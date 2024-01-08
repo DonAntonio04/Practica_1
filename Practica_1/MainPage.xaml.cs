f50259a53b0bd659213d14fb7db8cf2949a36e91
DonAntonio04 <berrellezantonio04@gmail.com>
2024-01-08 14:08:23 -0700

    nuevo

---
+++ Practica_1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practica_1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Refresh.IsRefreshing = true;
+            Thread.Sleep(200);
+            Refresh.IsRefreshing = false;
+        }
+    }
+}
